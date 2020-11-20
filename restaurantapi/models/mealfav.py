8ce6fc29a8d312eb45e391504dfb4bbf29f87b10
mbrownlee <michelle@orbwave.com>
2020-11-20 16:31:44 -0600

    edited some models, seeded DB

--- restaurantapi/models/mealfav.py
+++ restaurantapi/models/mealfav.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class MealFav(models.Model):
-    name = models.CharField(max_length=55)
-    restaurant = models.ForeignKey("Restaurant", on_delete=models.CASCADE)
+    meal = models.ForeignKey("Meal", on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     
